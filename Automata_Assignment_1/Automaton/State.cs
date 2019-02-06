1c2f54cb729cdcb3214f19237e1c4cb8386e8ad7
Frenken,Martin M.T <martin.frenken@student.fontys.nl>
2019-02-06 17:41:28 +0100

    Add project files.

---
+++ Automata_Assignment_1/Automaton/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata_Assignment_1.Automaton
+{
+    public class State
+    {
+        private string StateName { get; }
+        private bool IsBeginState { get; }
+        private bool IsEndState { get; }
+
+        public State(string stateName)
+        {
+            StateName = stateName;
+            IsBeginState = false;
+            IsEndState = false;
+        }
+        public State(string stateName,bool ce)
+        {
+            StateName = stateName;
+            IsBeginState = false;
+            IsEndState = false;
+        }
+    }
+}
---
+++ Automata_Assignment_1/Automaton/Transition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata_Assignment_1.Automaton
+{
+    public class Transition
+    {
+        private State InitialState { get; }
+        private State DestinationState { get; }
+        private char InputCharacter { get; }
+
+        public Transition(State initialState,State destinationState,char inputCharacter)
+        {
+            InitialState = initialState;
+            DestinationState = destinationState;
+            InputCharacter = inputCharacter;
+        }
+        
+    }
+}
